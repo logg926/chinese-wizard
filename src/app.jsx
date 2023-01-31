530622d72887e113f865d41a7f8139f466355bdd
Glitch (glitch-blank-react) <none>
2023-01-31 08:26:20 +0000

    🐇😎 Checkpoint ./src/app.jsx:19098585/3331

--- src/app.jsx
+++ src/app.jsx
@@ -1,43 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function App() {
-  const []useState=
+  const [result, setResult] = useState(["hi"]);
   return (
     <>
       <header>
-        <h2>Chinese Writing Wizard</h2>
-        <p>{"<Description>"}</p>
-        <div class="grid">
-          <select>
-            <option value="" disabled selected>
-              Select
-            </option>
-            <option>1</option>
-            <option>1</option>
-            <option>1</option>
-            <option>1</option>
-          </select>
-          <input
-            type="text"
-            name="firstname"
-            placeholder="First name"
-            aria-label="First name"
-            required
-          />
-          <input
-            type="email"
-            name="email"
-            placeholder="Email address"
-            aria-label="Email address"
-            required
-          />
-          <button type="submit">Generate</button>
-        </div>
+        <form
+          onSubmit={(event) => {
+            console.log(event);
+          }}
+        >
+          <h2>Chinese Writing Wizard</h2>
+          <p>{"<Description>"}</p>
+          <h6>ABCD:</h6>
+          <div class="grid">
+            <select>
+              <option value="" disabled selected>
+                Select
+              </option>
+              <option>1</option>
+              <option>1</option>
+              <option>1</option>
+              <option>1</option>
+            </select>
+            <h6>ABCD:</h6>
+            <input
+              type="text"
+              name="firstname"
+              placeholder="First name"
+              aria-label="First name"
+              required
+            />
+            <h6>ABCD:</h6>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email address"
+              aria-label="Email address"
+              required
+            />
+            <button type="submit">Generate</button>
+          </div>
+        </form>
       </header>
       <main class="container">
         <section id="Result">
           <h2>Result</h2>
-          
+          {result.map((result) => {
+            return (
+              <article>
+                <p>{result}</p>
+              </article>
+            );
+          })}
         </section>
       </main>
     </>
