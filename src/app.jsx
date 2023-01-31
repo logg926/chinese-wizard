23f9cac9f1a0c7db9ea6e9f86acaf3d5428773f6
Glitch (glitch-blank-react) <none>
2023-01-31 09:50:20 +0000

    👷🎱 Checkpoint ./src/app.jsx:19098585/204:54022717/957

--- src/app.jsx
+++ src/app.jsx
@@ -6,12 +6,16 @@ export default function App() {
     <>
       <header>
         <form
+          
           onSubmit={(event) => {
+            event.preventDefault();
             //call gpt 3
+            console.log(event.target);
+            
+            
             setResult((pre) => {
               return [...pre, "hi"];
             });
-            console.log(JSON.stringify(event));
           }}
         >
           <h2>Chinese Writing Wizard</h2>
@@ -21,46 +25,6 @@ export default function App() {
             <select>
               <option value="" disabled>學習重點</option>
               <option value="動態描寫">動態描寫</option>
-              <option 靜態描寫</option>
-              <option>白描法</option>
-              <option>細描法</option>
-              <option>定點描寫</option>
-              <option>感官描寫</option>
-              <option>視覺描寫</option>
-              <option>聽覺描寫</option>
-              <option>觸覺描寫</option>
-              <option>味覺描寫</option>
-              <option>嗅覺描寫</option>
-              <option>肖像描寫</option>
-              <option>心理描寫</option>
-              <option>行動描寫</option>
-              <option>語言描寫</option>
-              <option>神態描寫</option>
-              <option>襯托</option>
-                環境烘托
-                人物烘托
-                比喻（明喻、暗喻、借喻）
-擬人
-反覆
-呼吿
-誇張
-聯想
-排比
-對比
-對偶
-互文
-頂真
-象徵
-設問
-反問
-疊字
-層遞
-雙聲
-疊韻
-雙關
-通感
-顏色詞
-示現
 
             </select>
             <h6>題目:</h6>
