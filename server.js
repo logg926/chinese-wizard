1430a3afa2694ef2818b46bc1909da0a796c8ef4
Glitch (happy-chinese-wizard) <none>
2023-01-31 13:36:33 +0000

    🎪👗 Checkpoint ./server.js:19098585/2 ./src/app.jsx:19098585/196

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("./build"));
 
-var server = app.listen(3000, function () {
+var server = app.listen(3001, function () {
   console.log("Listening on port %s", server.address().port);
 });
