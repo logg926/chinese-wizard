519249761fc59749e4357b75875193d5e409bf91
Glitch (glitch-blank-react) <none>
2023-01-31 08:14:20 +0000

    🚃🌟 Checkpoint ./src/app.jsx:19098585/9175 ./index.html:19098585/36

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,45 @@
 import React from "react";
 
 export default function App() {
+  const []useState=
   return (
     <>
-      <form>
-        <input type="text" />
-        <button type="submit">Action</button>
-      </form>
+      <header>
+        <h2>Chinese Writing Wizard</h2>
+        <p>{"<Description>"}</p>
+        <div class="grid">
+          <select>
+            <option value="" disabled selected>
+              Select
+            </option>
+            <option>1</option>
+            <option>1</option>
+            <option>1</option>
+            <option>1</option>
+          </select>
+          <input
+            type="text"
+            name="firstname"
+            placeholder="First name"
+            aria-label="First name"
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email address"
+            aria-label="Email address"
+            required
+          />
+          <button type="submit">Generate</button>
+        </div>
+      </header>
+      <main class="container">
+        <section id="Result">
+          <h2>Result</h2>
+          
+        </section>
+      </main>
     </>
   );
 }
